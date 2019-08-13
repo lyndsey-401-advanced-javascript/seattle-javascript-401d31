b431c7aaa34406fa4b9d2aff671364c547c386f2
LYNDSEY THOMAS <THOMAS.LYNDSEY@GMAIL.COM>
2019-08-13 15:17:18 -0700

    lab02 begun

--- curriculum/class-01/lab/starter-code/__tests__/validator.test.js
+++ curriculum/class-01/lab/starter-code/__tests__/validator.test.js
@@ -2,6 +2,30 @@
 
 const validator = require('../lib/validator.js');
 
+
+describe('#validator', () => {
+  describe('#isLessThanTen', () => {
+    test('regular cases', () => {
+      expect(validator.isLessThanTen(5)).toEqual(true);
+    });
+  });
+});
+
+describe('#isObjectValid', () => {
+  test ('regular cases', () => {
+    const schema = {
+      fields: {
+        id: {type: 'string'}, //if there is an id it HAS to be a string
+        age: {type: 'number'}, //if there is an age it HAS to be a number etc 
+        favoriteToys: {type: 'object'},
+      },
+    };
+    expect(validator.isObjectValid(
+      data:{id: 'kali', age: 2, favoriteToys: {}}, schema)
+  }).toEqual(true);
+});
+
+
 describe('validator module performs basic validation of', () => {
 
   // TODO: Make this series of tests less repetitive ... DRY it out
--- curriculum/class-01/lab/starter-code/index.js
+++ curriculum/class-01/lab/starter-code/index.js
@@ -1,3 +1,18 @@
 'use strict';
 
 const validator = require('./lib/validator.js');
+
+
+
+'use strict';
+
+const validator = require('./lib/validator');
+
+
+let str = 'yes';
+let num = 1;
+let arr = ['a'];
+let obj = { x: 'y' };
+let func = () => { };
+let bool = false;
+
