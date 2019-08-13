49dff9e78e4b2a6ef2c1bbbf304aa44be46748d4
LYNDSEY THOMAS <THOMAS.LYNDSEY@GMAIL.COM>
2019-08-13 16:25:30 -0700

    lab01 tests suite

--- curriculum/class-01/lab/starter-code/__tests__/validator.test.js
+++ curriculum/class-01/lab/starter-code/__tests__/validator.test.js
@@ -1,71 +1,49 @@
 'use strict';
 
-const validator = require('../lib/validator.js');
+const validator = require('../lib/validator');
 
-describe('validator module performs basic validation of', () => {
-
-  // TODO: Make this series of tests less repetitive ... DRY it out
-
-  it('strings', () => {
-    let str = 'yes';
-    let num = 1;
-    let arr = ['a'];
-    let obj = {x:'y'};
-    let func = () => {};
-    let bool = false;
-    expect(validator.isString(str)).toBeTruthy();
-    expect(validator.isString(num)).toBeFalsy();
-    expect(validator.isString(arr)).toBeFalsy();
-    expect(validator.isString(obj)).toBeFalsy();
-    expect(validator.isString(func)).toBeFalsy();
-    expect(validator.isString(bool)).toBeFalsy();
+//lthomas - this is the solution code that Vinicio covered in class 
+describe('#validator', () => {
+  describe('#isLessThanTen', () => {
+    test('normal cases', () => {});
+    expect(validator.isLessThanTen(5)).toEqual(true);
+    expect(validator.isLessThanTen(11)).toEqual(false);
+    expect(validator.isLessThanTen(10)).toEqual(false);
+    expect(validator.isLessThanTen(-1)).toEqual(true);
   });
+}); 
 
-  it('numbers', () => {
-    expect(true).toBeFalsy();
+describe('#isObjectValid', () => {
+  test('normal cases', () => {
+    const schema = {
+      fields: {
+        id: {type: 'string'},
+        age: {type: 'number'},
+        favoriteToys: {type: 'object'},
+      },
+    };
+    expect(validator.isObjectValid(
+      data: {id:'Irish', age: 11, favoriteToys: {}}, schema
+    )).toEqual(true);
+    expect(validator.isObjectValid(
+      data: {id: NaN, age: 'helloWorld', favoriteToys: []}, schema
+    )).toEqual(false);
   });
+}); 
 
-  it('arrays', () => {
-    expect(true).toBeFalsy();
-  });
+//goals: Can you test for number? 
+//Can you test for type?
+//Can you test for property rules? 
 
-  it('objects', () => {
-    expect(true).toBeFalsy();
-  });
+describe('#isString', () => {
+  test('normal cases', () => {});
+  expect(validator.isString('hello')).toEqual(true);
+  expect(validator.isString("Hello World")).toEqual(true);
+  expect(validator.isString(4)).toEqual(false);
+  expect(validator.isString([])).toEqual(false);
+})
 
-  it('booleans', () => {
-    expect(true).toBeFalsy();
-  });
 
-  it('functions', () => {
-    expect(true).toBeFalsy();
-  });
 
-});
 
-describe('validator module performs complex validations', () => {
-
-  it('validates the presence of required object properties at any level', () => {
-    // i.e. does person.hair.color exist and have a good value, not just person.hair
-    expect(true).toBeFalsy();
-  });
-
-  it('validates the proper types of object properties', () => {
-    // i.e. person.name must be a string, etc.
-    expect(true).toBeFalsy();
-  });
-
-  it('validates the types of values contained in an array', () => {
-    // i.e. an array of all strings or numbers
-    expect(true).toBeFalsy();
-  });
-
-  it('validates a value array against an approved list', () => {
-    // i.e. a string might only be allowed to be "yes" or "no"
-    expect(true).toBeFalsy();
-  });
-
-  // TODO: Cover so, so many more cases
-
-});
 
--- curriculum/class-01/lab/starter-code/lib/validator.js
+++ curriculum/class-01/lab/starter-code/lib/validator.js
@@ -1,23 +1,28 @@
 'use strict';
 
+//setting up object that can be pushed out to test folder, can do validator. iot call tests 
 let validator = module.exports = {};
 
-/**
- * Based on a set of rules, is the input valid?
- * TODO: Define the rules ... how do we send them in? How do we identify?
- * @param input
- * @param rules
- * @returns {boolean}
- */
-validator.isValid = (input, rules) => {
-  return true;
+//lthomas - this is the solution code that Vinicio covered in class 13 Aug
+validator.isLessThanTen = (input) => {
+  return input < 10; 
 };
 
-/**
- * Is this a string?
- * @param input
- * @returns {boolean}
- */
+validator.isTypeFunction = (input) => {};
+
+validator.isObjectValid = (data, schema) => {
+  //data will be object
+  //schema will be object that has property called fields
+  //validate? 
+  Object.keys(schema.fields).forEact(callbackfn: expectedProperty => {
+    if (data.hasOwnProperty(expectedProperty)){
+      return false;
+    }
+    //TODO: Add type validation 
+  });
+  return true; 
+};
+
 
 validator.isString = (input) => {
   return typeof input === 'string';
